2b5f1ff94ee1b6ce260c6cdcac5e9abee4ed3f4c
Natalya <natalya-pilchuk@mail.ru>
2022-08-31 11:12:07 +0300

    Booking_Homework18_new

--- src/test/java/steps/SearchSteps.java
+++ src/test/java/steps/SearchSteps.java
@@ -1,44 +1,62 @@
 package steps;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.selector.ByText;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import pages.HotelsPage;
+import pages.MainPage;
+
+import java.awt.*;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchSteps {
+    MainPage mainPage;
+    HotelsPage hotelsPage;
+
+    @Before
+    public void setUp() {
+        Configuration.timeout = 6000;
+        Dimension size
+                = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) size.getWidth();
+        int height = (int) size.getHeight() - 50;
+        Configuration.browserSize = String.format("%dx%d", width, height);
+        mainPage = new MainPage();
+        hotelsPage = new HotelsPage();
+
+    }
 
     @Given("Main Booking page opened")
     public void mainBookingPageOpened() {
-        open("https://www.booking.com/searchresults.en-gb.html");
+        mainPage.openMainPage();
     }
 
     @When("User input the name of hotel{string}")
     public void userInputTheNameOfHotel(String hotelName) {
-        String hotelXpath = String.format("//div[contains(@data-testid,'searchbox-layout-vertical')]//input[@placeholder='Where are you going?']", hotelName);
-        $(By.xpath(hotelXpath)).shouldBe(Condition.visible).sendKeys("404 Rooms Wilanowska");
+        mainPage.inputTheNameOfHotel(hotelName);
     }
 
     @And("User click button Search")
     public void userClickButtonSearch() {
-        $(By.xpath("//div[contains(@data-testid,'searchbox-layout-vertical')]//button[@type='submit']")).click();
+        mainPage.clickSearch();
     }
 
     @Then("hotel {string}presents on page")
     public void hotelPresentsOnPage(String nameOfHotel) {
-        String nameHotel = $(new ByText("404 Rooms Wilanowska: 1 property found")).getText();
-        Assert.assertEquals(nameHotel, "404 Rooms Wilanowska: 1 property found", "there isn't any hotels with such name");
+        Assert.assertEquals(hotelsPage.hotelPresentsOnPage(nameOfHotel), "Castle Inn", "there isn't any hotels with such name");
     }
 
 
-    @Then("hotel have right rating")
-    public void hotelHaveRating() {
-        String rating = $(By.xpath("//div[contains(@data-testid,'property-card')]//div[@aria-label='Scored 8.3 '][1]")).getText();
-        Assert.assertEquals(rating, "8.3", "the rating is wrong");
+    @Then("hotel has {string} rating")
+    public void hotelHasRating(String ratingOfHotel) {
+        Assert.assertEquals(hotelsPage.rightRatingPresentsOnPage(ratingOfHotel), "8.5", "the rating of hotel is wrong");
     }
 }
--- src/test/resources/feature/search.feature
+++ src/test/resources/feature/search.feature
@@ -2,7 +2,7 @@ Feature: Search
 
   Scenario: Search the hotel
     Given Main Booking page opened
-    When User input the name of hotel"404 Rooms Wilanowska"
+    When User input the name of hotel"Castle Inn"
     And User click button Search
-    Then  hotel "404 Rooms Wilanowska"presents on page
-    Then hotel have right rating
+    Then  hotel "Castle Inn"presents on page
+    Then hotel has "8.5" rating
